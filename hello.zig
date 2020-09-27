0f2d32ddba863496324a9e402efd09f4d5f5125a
Adam H <adam@roboadam.com>
2020-09-27 15:18:23 -0400

    Need to link

--- hello.zig
+++ hello.zig
@@ -1,10 +1,11 @@
 const std = @import("std");
+const print = @import("std").debug.print;
 const c = @cImport(@cInclude("GLFW/glfw3.h"));
 
 const glfw = @import("glfw");
 
 pub fn main() void {
-    if (c.glfwInit()) {
+    if (c.glfwInit() == 0) {
         print("Hello, world!\n", .{});
     }
 }
