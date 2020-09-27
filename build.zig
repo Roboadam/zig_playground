b67b8d3fdddbe7171026b603d0ec60a52d2cfa38
Adam H <adam@roboadam.com>
2020-09-27 19:31:31 -0400

    mroe linker stuff

--- build.zig
+++ build.zig
@@ -3,6 +3,10 @@ const Builder = @import("std").build.Builder;
 pub fn build(b: *Builder) void {
     const exe = b.addExecutable("hello", "hello.zig");
     exe.setBuildMode(b.standardReleaseOptions());
+    exe.addIncludeDir("C:\\Users\\adam\\dev\\glfw\\glfw-3.3.2\\include");
+    exe.addLibPath("C:\\Users\\adam\\dev\\glfw\\glfw-3.3.2\\build\\src\\Release");
+
     exe.linkSystemLibrary("c");
+    exe.linkSystemLibrary("glfw3");
     b.default_step.dependOn(&exe.step);
 }
