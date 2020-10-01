0f838738a9fabffba4d4e0353dc0e3097efba97d
Adam H <adam@roboadam.com>
2020-09-30 23:23:09 -0400

    I honestly never thought i'd get this far

--- build.zig
+++ build.zig
@@ -4,11 +4,12 @@ pub fn build(b: *Builder) void {
     const exe = b.addExecutable("hello", "hello.zig");
     exe.setBuildMode(b.standardReleaseOptions());
     exe.addIncludeDir("C:\\Users\\adam\\dev\\glfw\\glfw-3.3.2\\include");
+    exe.addIncludeDir("C:\\Users\\adam\\dev\\vulkan\\1.2.148.1\\Include");
     exe.addLibPath("C:\\Users\\adam\\dev\\glfw\\glfw-3.3.2\\src\\Release");
 
     exe.linkSystemLibrary("glfw3");
     exe.linkSystemLibrary("c");
-    exe.linkSystemLibrary("opengl32");
+    exe.linkSystemLibrary("C:\\Users\\adam\\dev\\vulkan\\1.2.148.1\\Lib\\vulkan-1");
     exe.linkSystemLibrary("user32");
     exe.linkSystemLibrary("gdi32");
     exe.linkSystemLibrary("shell32");
--- hello.zig
+++ hello.zig
@@ -1,8 +1,19 @@
 const print = @import("std").debug.print;
-const c = @cImport(@cInclude("GLFW/glfw3.h"));
-const glfw = @import("glfw");
+const c = @cImport({
+    @cDefine("GLFW_INCLUDE_VULKAN", {});
+    @cInclude("GLFW/glfw3.h");
+});
 
 pub fn main() void {
     const result = c.glfwInit();
     print("result:{} true is {}", .{ result, c.GLFW_TRUE });
+    c.glfwWindowHint(c.GLFW_CLIENT_API, c.GLFW_NO_API);
+
+    // GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    const window = c.glfwCreateWindow(800, 600, "Vulkan window", null, null);
+
+    // uint32_t extensionCount = 0;
+    // vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    var extensionCount: u32 = 0;
+    const blah = c.vkEnumerateInstanceExtensionProperties(null, &extensionCount, null);
 }
