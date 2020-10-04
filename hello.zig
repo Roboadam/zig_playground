36ddf23356b10c9d4ce2ee0fda3cffa33b2c7370
Adam H <adam@roboadam.com>
2020-10-04 13:35:59 -0400

    neat

--- hello.zig
+++ hello.zig
@@ -7,25 +7,25 @@ const c = @cImport({
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
 
-pub fn main() void {
+pub fn main() !void {
     var app = HelloTriangleApplication{
         .window = null,
-        .instance = null,
     };
-    app.run();
+    try app.run();
 
     // var extensionCount: u32 = 0;
     // _ = c.vkEnumerateInstanceExtensionProperties(null, &extensionCount, null);
     // print("{} extensions supported\n", .{extensionCount});
 }
 
+var instance: c.VkInstance = undefined;
+
 const HelloTriangleApplication = struct {
     window: ?*c.GLFWwindow,
-    instance: ?*c.VkInstance,
 
-    pub fn run(self: *HelloTriangleApplication) void {
+    pub fn run(self: *HelloTriangleApplication) !void {
         self.initWindow();
-        self.initVulkan();
+        try self.initVulkan();
         self.mainLoop();
         self.cleanup();
     }
@@ -45,8 +45,8 @@ const HelloTriangleApplication = struct {
         self.window = c.glfwCreateWindow(WIDTH, HEIGHT, "Vulkan window", null, null);
     }
 
-    fn initVulkan(self: HelloTriangleApplication) void {
-        self.createInstance();
+    fn initVulkan(self: HelloTriangleApplication) !void {
+        try self.createInstance();
     }
 
     fn mainLoop(self: HelloTriangleApplication) void {
@@ -60,14 +60,48 @@ const HelloTriangleApplication = struct {
         c.glfwTerminate();
     }
 
-    fn createInstance(self: HelloTriangleApplication) void {
+    fn createInstance(self: HelloTriangleApplication) !void {
+        // This data is technically optional, but it may provide some useful
+        // information to the driver in order to optimize our specific
+        // application (e.g. because it uses a well-known graphics engine with
+        // certain special behavior).
         const appInfo = c.VkApplicationInfo{
-            .sType = c.VK_STRUCTURE_TYPE_APPLICATION_INFO,
+            .sType = c.VkStructureType.VK_STRUCTURE_TYPE_APPLICATION_INFO,
             .pApplicationName = "Hello Triangle",
             .applicationVersion = c.VK_MAKE_VERSION(1, 0, 0),
             .pEngineName = "No Engine",
             .engineVersion = c.VK_MAKE_VERSION(1, 0, 0),
             .apiVersion = c.VK_API_VERSION_1_0,
+            .pNext = null,
         };
+
+        // This next struct is not optional and tells the Vulkan driver which
+        // global extensions and validation layers we want to use. Global here
+        // means that they apply to the entire program and not a specific
+        // device. The first two parameters are straightforward. The next two
+        // layers specify the desired global extensions. As mentioned in the
+        // overview chapter, Vulkan is a platform agnostic API, which means
+        // that you need an extension to interface with the window system. GLFW
+        // has a handy built-in function that returns the extension(s) it needs
+        // to do that which we can pass to the struct
+        var glfwExtensionCount: u32 = 0;
+        var glfwExtensions = c.glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        const createInfo = c.VkInstanceCreateInfo{
+            .sType = c.VkStructureType.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            .pApplicationInfo = &appInfo,
+            .enabledExtensionCount = glfwExtensionCount,
+            .ppEnabledExtensionNames = glfwExtensions,
+            .enabledLayerCount = 0,
+            .pNext = null,
+            .flags = 0,
+            .ppEnabledLayerNames = null,
+        };
+
+        print("1", .{});
+        if (c.vkCreateInstance(&createInfo, null, &instance) != c.VkResult.VK_SUCCESS) {
+            print("2", .{});
+            return error.What;
+        }
+        print("3", .{});
     }
 };
