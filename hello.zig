78aad1148cfcd91f047686ac3eefc35b876d92bc
Adam H <adam@roboadam.com>
2020-10-20 20:52:49 -0400

    stash

--- hello.zig
+++ hello.zig
@@ -143,6 +143,22 @@ const HelloTriangleApplication = struct {
 
         return true;
     }
+
+    // std::vector<const char*> getRequiredExtensions() {
+    fn getRequiredExtensions() [][*]const u8 {
+        //     uint32_t glfwExtensionCount = 0;
+        var glfwExtensionCount: u32 = 0;
+        //     const char** glfwExtensions;
+        //     glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        //     std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        //     if (enableValidationLayers) {
+        //         extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        //     }
+
+        //     return extensions;
+    }
 };
 
 fn fixNullTerminatedString(input: []const u8) []const u8 {
